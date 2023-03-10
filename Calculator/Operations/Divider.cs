cc17d7e2d6acc28accb509b3eb5d184ffdb58b17
alirezaeiji <alirezaeiji151479@gmail.com>
2023-03-10 19:06:58 +0330

    Add Divider

---
+++ Calculator/Operations/Divider.cs
@@ -0,0 +1,12 @@
+﻿using Calculator.Abstractions;
+
+namespace Calculator.Operations
+{
+    public class Divider : IDivider
+    {
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
